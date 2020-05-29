06d6f839ea3e24e5a5eef45ec4f52003db461cca
robert0999 <beto25861@gmail.com>
2020-05-29 12:43:22 -0500

    Initial commit

---
+++ app/src/main/java/topete/salazar/mycalculador/MainActivity.kt
@@ -0,0 +1,12 @@
+package topete.salazar.mycalculador
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
